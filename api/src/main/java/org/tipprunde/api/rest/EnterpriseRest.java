ae07e04fbbe8fca99599364f8b0b0c85677a511b
Thorsten-K <t.kisner@web.de>
2018-10-28 20:13:19 +0100

    REST secured

--- api/src/main/java/org/tipprunde/api/rest/EnterpriseRest.java
+++ api/src/main/java/org/tipprunde/api/rest/EnterpriseRest.java
@@ -9,6 +9,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.jeesl.api.exception.xml.JeeslXmlStructureException;
+import org.jeesl.interfaces.util.qualifier.JeeslRestSecured;
 import org.tipprunde.model.xml.community.Bet;
 import org.tipprunde.model.xml.community.Community;
 import org.tipprunde.model.xml.community.DefinitionEvent;
@@ -24,67 +25,67 @@ import net.sf.ahtutils.exception.ejb.UtilsNotFoundException;
 @Path("/rest/enterprise")
 public interface EnterpriseRest
 {
-    @GET @Path("/community/members/{id:[1-9][0-9]*}")
+    @GET @Path("/community/members/{id:[1-9][0-9]*}") @JeeslRestSecured
     @Produces(MediaType.APPLICATION_XML)
     Community members(@PathParam("id") long communityId);
     
-    @GET @Path("/participant/{id:[1-9][0-9]*}/{email}")
+    @GET @Path("/participant/{id:[1-9][0-9]*}/{email}") @JeeslRestSecured
     @Produces(MediaType.APPLICATION_XML)
     Participant participant(@PathParam("id") long communityId, @PathParam("email") String email);
     
-    @GET @Path("/event/{id:[1-9][0-9]*}")
+    @GET @Path("/event/{id:[1-9][0-9]*}") @JeeslRestSecured
     @Produces(MediaType.APPLICATION_XML)
     DefinitionEvent event(@PathParam("id") long communityId);
     
-    @GET @Path("/community/opponents/{id:[1-9][0-9]*}")
+    @GET @Path("/community/opponents/{id:[1-9][0-9]*}") @JeeslRestSecured
     @Produces(MediaType.APPLICATION_XML)
     Opponents opponents(@PathParam("id") long communityId);
     
-    @POST @Path("/subscribe")
+    @POST @Path("/subscribe") @JeeslRestSecured
     @Consumes(MediaType.APPLICATION_XML) @Produces(MediaType.APPLICATION_XML)
 	Participant subscribeParticipant(Participant participant);
     
-    @POST @Path("/unsubscribe")
+    @POST @Path("/unsubscribe") @JeeslRestSecured
     @Consumes(MediaType.APPLICATION_XML) @Produces(MediaType.APPLICATION_XML)
 	Participant unsubscribeParticipant(Participant participant);
     
-    @POST @Path("/bet")
+    @POST @Path("/bet") @JeeslRestSecured
     @Consumes(MediaType.APPLICATION_XML) @Produces(MediaType.APPLICATION_XML)
 	Bet updateBet(Bet bet) throws JeeslXmlStructureException;
 	
-    @GET @Path("/tipps/{drId:[1-9][0-9]*}/{pId:[1-9][0-9]*}")
+    @GET @Path("/tipps/{drId:[1-9][0-9]*}/{pId:[1-9][0-9]*}") @JeeslRestSecured
     @Produces(MediaType.APPLICATION_XML)
     Tipps tipps(@PathParam("drId") long defRoundId, @PathParam("pId") long participantId);
     
-    @POST @Path("/update")
+    @POST @Path("/update") @JeeslRestSecured
     @Consumes(MediaType.APPLICATION_XML) @Produces(MediaType.APPLICATION_XML)
     Tipps update(Tipps tipps) throws JeeslXmlStructureException, UtilsNotFoundException;
 
-    @GET @Path("/update/worker")
+    @GET @Path("/update/worker") @JeeslRestSecured
     @Produces(MediaType.APPLICATION_XML)
     Tg updateWorker();
     
-    @GET @Path("/updatePoints/{drId:[1-9][0-9]*}")
+    @GET @Path("/updatePoints/{drId:[1-9][0-9]*}") @JeeslRestSecured
     @Produces(MediaType.TEXT_PLAIN)
     String updatePoints(@PathParam("drId") long defRoundId);
     
-    @GET @Path("/roundScoreTable/{drId:[1-9][0-9]*}")
+    @GET @Path("/roundScoreTable/{drId:[1-9][0-9]*}") @JeeslRestSecured
     @Produces(MediaType.APPLICATION_XML)
 	public Report roundScoreTable(@PathParam("drId") long defRoundId);
     
-    @GET @Path("/missingTipps/{drId:[1-9][0-9]*}")
+    @GET @Path("/missingTipps/{drId:[1-9][0-9]*}") @JeeslRestSecured
     @Produces(MediaType.APPLICATION_XML)
 	public Mails missingTipps(@PathParam("drId") long defRoundId);
     
-    @GET @Path("/mails/roundScoreTable/{drId:[1-9][0-9]*}")
+    @GET @Path("/mails/roundScoreTable/{drId:[1-9][0-9]*}") @JeeslRestSecured
     @Produces(MediaType.APPLICATION_XML)
 	public Mails mailsRoundScoreTable(@PathParam("drId") long defRoundId);
     
-    @GET @Path("/mail/round/reminder/{drId:[1-9][0-9]*}")
+    @GET @Path("/mail/round/reminder/{drId:[1-9][0-9]*}") @JeeslRestSecured
     @Produces(MediaType.APPLICATION_XML)
 	Mails mailRoundReminder(@PathParam("drId") long defRoundId);
     
-    @GET @Path("/mail/round/score/{drId:[1-9][0-9]*}")
+    @GET @Path("/mail/round/score/{drId:[1-9][0-9]*}") @JeeslRestSecured
     @Produces(MediaType.APPLICATION_XML)
 	Mails mailRoundScore(@PathParam("drId") long defRoundId);
 }
--- api/src/main/java/org/tipprunde/api/rest/UpdateRest.java
+++ api/src/main/java/org/tipprunde/api/rest/UpdateRest.java
@@ -7,26 +7,27 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.jeesl.interfaces.util.qualifier.JeeslRestSecured;
 import org.tipprunde.model.xml.liga.UpdatePolicy;
 import org.tipprunde.model.xml.tr.Tg;
 
 @Path("/rest/update")
 public interface UpdateRest
 {
-    @GET @Path("/next")
+    @GET @Path("/next") @JeeslRestSecured
     @Produces(MediaType.APPLICATION_XML)
     UpdatePolicy nextUpdate();
     
-	@GET @Path("/job")
+	@GET @Path("/job") @JeeslRestSecured
 	@Produces(MediaType.APPLICATION_XML)
     Tg nextJob();
 	
-	@POST @Path("/update")
+	@POST @Path("/update") @JeeslRestSecured
 	@Consumes(MediaType.APPLICATION_XML)
 	@Produces(MediaType.APPLICATION_XML)
     UpdatePolicy update(Tg tg);
 	
-	@POST @Path("/status")
+	@POST @Path("/status") @JeeslRestSecured
 	@Consumes(MediaType.APPLICATION_XML)
     void status(UpdatePolicy status);
 }
