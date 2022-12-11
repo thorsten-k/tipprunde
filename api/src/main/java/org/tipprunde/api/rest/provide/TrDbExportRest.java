9e2ee5823bed6279e183abef88ea96d5e34abbd2
Thorsten-K <t.kisner@web.de>
2022-12-11 09:39:49 +0100

    changes of jeesl applied

--- api/src/main/java/org/tipprunde/api/rest/provide/TrDbExportRest.java
+++ api/src/main/java/org/tipprunde/api/rest/provide/TrDbExportRest.java
@@ -2,7 +2,7 @@ package org.tipprunde.api.rest.provide;
 
 import javax.ws.rs.Path;
 
-import org.jeesl.api.rest.system.security.JeeslSecurityRestExport;
+import org.jeesl.api.rest.rs.system.security.JeeslSecurityRestExport;
 
 @Path("/rest/db/export")
 public interface TrDbExportRest extends JeeslSecurityRestExport
--- api/src/main/java/org/tipprunde/api/rest/provide/TrDbImportRest.java
+++ api/src/main/java/org/tipprunde/api/rest/provide/TrDbImportRest.java
@@ -2,7 +2,7 @@ package org.tipprunde.api.rest.provide;
 
 import javax.ws.rs.Path;
 
-import org.jeesl.api.rest.system.security.JeeslSecurityRestImport;
+import org.jeesl.api.rest.rs.system.security.JeeslSecurityRestImport;
 
 @Path("/rest/db/import")
 public interface TrDbImportRest extends JeeslSecurityRestImport
