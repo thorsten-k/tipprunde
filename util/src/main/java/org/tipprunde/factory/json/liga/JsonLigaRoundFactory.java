2d76f3f0b91cb52b008cdd391afe9f33508b85f4
Thorsten-K <t.kisner@web.de>
2023-04-11 07:26:47 +0200

    season/shortcut added

--- util/src/main/java/org/tipprunde/factory/json/liga/JsonLigaRoundFactory.java
+++ util/src/main/java/org/tipprunde/factory/json/liga/JsonLigaRoundFactory.java
@@ -1,13 +1,20 @@
 package org.tipprunde.factory.json.liga;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.tipprunde.factory.json.JsonTrContainerFactory;
 import org.tipprunde.factory.json.ds.JsonDataSourceFactory;
 import org.tipprunde.factory.json.ds.JsonDsOpenligaFactory;
 import org.tipprunde.interfaces.ejb.liga.match.TipprundeDsOpenLiga;
+import org.tipprunde.model.json.JsonTrContainer;
 import org.tipprunde.model.json.ds.JsonDsOpenLiga;
 import org.tipprunde.model.json.liga.JsonLigaRound;
 import org.tipprunde.model.json.ssi.openliga.OpenLigaGroup;
+
+import net.sf.exlp.util.io.JsonUtil;
 
 public class JsonLigaRoundFactory
 {	
@@ -22,6 +29,21 @@ public class JsonLigaRoundFactory
 	public static JsonLigaRound build() {return new JsonLigaRound();}
 	
 	
+	public static JsonTrContainer transform(JsonDsOpenLiga job, List<OpenLigaGroup> list)
+	{
+		JsonTrContainer json = JsonTrContainerFactory.build();
+		json.setRounds(new ArrayList<>());
+		for(OpenLigaGroup olg : list)
+		{
+			if(job.getRefId().intValue()==olg.getId().intValue())
+			{
+				json.getRounds().add(JsonLigaRoundFactory.build(olg));
+			}
+		}
+		JsonUtil.info(json);
+		return json;
+	}
+	
 	public static JsonLigaRound build(OpenLigaGroup olg)
 	{
 		JsonLigaRound json = build();
@@ -32,6 +54,9 @@ public class JsonLigaRoundFactory
 		JsonDsOpenLiga ds = JsonDsOpenligaFactory.build();
 		ds.setType(TipprundeDsOpenLiga.Type.group.toString());
 		ds.setRefId(olg.getId().intValue());
+		
+		ds.setSeason(olg.getLeagueSeason());
+		ds.setShortcut(ds.getShortcut());
 		ds.setGroupOrderId(olg.getGroupOrderID());
 		
 		json.setDataSource(JsonDataSourceFactory.build(ds));
